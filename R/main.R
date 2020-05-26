bf96b9739af7d74c8e25f58042ff191dd0a9844f
Peng Zhao <baydap@gmail.com>
2020-05-26 11:56:29 +0800

    get_map() with amap

--- R/main.R
+++ R/main.R
@@ -13,10 +13,36 @@ get_fullname <- function(x = c("XJTLU", "AMO")){
 
 #' Plot a campus map of XJTLU
 #'
+#' @param english logical. Display English on the map.
+#'
 #' @return a map
 #' @export
 #'
-#' @examples get_map()
-get_map <- function(){
-  leaflet::setView(leaflet::addAwesomeMarkers(leaflet::addTiles(leaflet::leaflet()), 120.734, 31.275),120.735, 31.273, zoom = 15.4)
+#' @examples
+#' get_map()
+#' get_map(english = TRUE)
+get_map <- function(english = FALSE){
+  icon.fa <- leaflet::makeAwesomeIcon(icon = 'flag', markerColor = 'red', library='fa', iconColor = 'black')
+  if(english){
+    long <- 120.734
+    lat <- 31.275
+    link <- "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+    # see http://leaflet-extras.github.io/leaflet-providers/preview/
+    leaflet::setView(
+      leaflet::addAwesomeMarkers(
+        leaflet::addTiles(urlTemplate = link,
+                          leaflet::leaflet()),
+        long, lat, icon = icon.fa),
+      long, lat - 0.0015, zoom = 16)
+  } else{
+    long <- 120.7385
+    lat <- 31.273
+    leafletCN::amap(
+      leaflet::setView(
+        leaflet::addAwesomeMarkers(
+            leaflet::leaflet(),
+          long, lat, icon = icon.fa),
+        long, lat - 0.0015, zoom = 16)
+    )
+  }
 }
