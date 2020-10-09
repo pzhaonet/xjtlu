2a7aba6356336f9d25fadcd695dbdd6653044f29
pzhaonet <pzhao@pzhao.net>
2020-10-09 13:03:00 +0800

    fix a bug in download_bbb()

--- R/main.R
+++ R/main.R
@@ -63,44 +63,57 @@ get_map <- function(english = FALSE){
 #' @examples
 #' # download one
 #' download_bbb("https://bbbload.xjtlu.edu.cn/presentation/4c0413f756243e7b71221b81e36488f80d15996c-1599613283607/video/webcams.webm")
-#' # batch download
-#' env203 <- function(video){
-#'   paste0("https://bbbload.xjtlu.edu.cn/presentation/4c0413f756243e7b71221b81e36488f80d15996c-", video,"/video/webcams.webm")
-#' }
-#' urls <- env203(c("1599613283607", "1602031837942", "1600822173907", "1600217953595"))
-#' for (i in 1:4) {
-#'   download_bbb(urls[i],
-#'                camera_file = paste0("cam", i, ".webm"),
-#'                desktop_file = paste0("desk", i, ".webm"),
-#'                temp_file = paste0("tmp", i, ".webm"),
-#'                merged_file = paste0("bbb', i, '.webm"))
+#'
+#' # batch download and merge
+#' urls <- c("https://bbbload.xjtlu.edu.cn/presentation/4c0413f756243e7b71221b81e36488f80d15996c-1599613283607/video/webcams.webm",
+#'           "https://bbbload.xjtlu.edu.cn/presentation/4c0413f756243e7b71221b81e36488f80d15996c-1602031837942/video/webcams.webm",
+#'           "https://bbbload.xjtlu.edu.cn/presentation/4c0413f756243e7b71221b81e36488f80d15996c-1600217953595/video/webcams.webm")
+#' for (i in 1:3) {
+#'   download_bbb(
+#'     urls[i],
+#'     camera_file = paste0("cam_chapter_", i, ".webm"),
+#'     desktop_file = paste0("desk_chapter_", i, ".webm"),
+#'     temp_file = paste0("tmp_chapter_", i, ".webm"),
+#'     merged_file = paste0("bbb_chapter_", i, ".webm")
+#'   )
 #' }
 download_bbb <- function(url = NA,
                          camera_file = "webcams.webm",
                          desktop_file = "desktop.webm",
                          temp_file = "tmp.webm",
                          merged_file = NA){
+  report <- data.frame(Mission = NULL, Start = NULL, End = NULL, Time = NULL)
   if (!is.na(url)){
     id <- gsub("^.+presentation/(.+)/video/webcams.webm", "\\1", url)
     url_desktop <- paste0("https://bbbload.xjtlu.edu.cn/presentation/", id, "/deskshare/deskshare.webm")
 
-    message("Downloading the camera recoding...")
+    start_time <- Sys.time()
+    message(start_time, ": Downloading the camera recoding...")
     download.file(url = url, destfile = camera_file, method = "curl")
-    if (file.exists(camera_file)) message("Camera recoding downloaded!")
+    end_time <- Sys.time()
+    if (file.exists(camera_file)) message(end_time, ": Camera recoding downloaded!")
+    report <- rbind(report, c(camera_file, as.character(start_time), as.character(end_time), round(difftime(end_time, start_time, units = "min"), 1)))
 
-    message("Downloading the shared desktop...")
+    start_time <- Sys.time()
+    message(start_time, ": Downloading the shared desktop...")
     download.file(url = url_desktop, destfile =  desktop_file, method = "curl")
-    if (file.exists(desktop_file)) message("Share desktop downloaded!")
+    end_time <- Sys.time()
+    if (file.exists(desktop_file)) message(end_time, ": Share desktop downloaded!")
+    report <- rbind(report, c(desktop_file, as.character(start_time), as.character(end_time), round(difftime(end_time, start_time, units = "min"), 1)))
   }
 
   if (!is.na(merged_file)){
-    message("Merging videos...")
+    start_time <- Sys.time()
+    message(start_time, ": Merging videos...")
     system(paste0('ffmpeg -i ', desktop_file, ' -vf "movie=', camera_file,', scale=300:-1 [inner]; [in][inner] overlay=1000:500 [out]" ', temp_file))
     system(paste0('ffmpeg -i ', temp_file,' -i ', camera_file, ' -c copy ', merged_file))
+    end_time <- Sys.time()
     if (file.exists(merged_file)) {
-      message("Videos merged!")
+      message(Sys.time(), ": Videos merged!")
       file.remove(temp_file)
+      report <- rbind(report, c(merged_file, as.character(start_time), as.character(end_time), round(difftime(end_time, start_time, units = "min"), 1)))
     }
   }
-  message("Mission accomplished!")
+  message(Sys.time(), ": Mission accomplished!")
+  if (nrow(report) > 0) print(report)
 }
